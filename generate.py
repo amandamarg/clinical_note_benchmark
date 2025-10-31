ef9e3821dba6aee65e47e76834408b790986cf2f
amandamarg <ammargo@umich.edu>
2025-10-31 15:03:28 -0400

    updated README.md

--- generate.py
+++ generate.py
@@ -3,14 +3,26 @@ from tqdm import tqdm
 import time
 import requester
 import pandas as pd
+import json
 
 '''
 Run this script to generate clinical notes using a specified model and prompt.
 '''
-
 GENERATE_MODEL_NAME = 'ozwell'
 GENERATE_PROMPT_NAME = 'g1'
 IDXS = [224, 431, 562, 619, 958, 1380, 1716, 1834, 2021, 3026, 3058, 3093, 3293, 3931, 4129] # or 'all'
+
+def init_dirs(df, root='./'):
+    for idx, row in tqdm(df.iterrows(), total=len(df), ncols=50):
+        if str(idx) not in os.listdir(os.path.join(root, 'results')):
+            os.makedirs(os.path.join(root, 'results', str(idx)))
+        for k,v in row.to_dict().items():
+            if k == 'summary' and not os.path.exists(os.path.join(root, 'results', str(idx), f'{k}.json')):
+                with open(os.path.join(root, 'results', str(idx), f'{k}.json'), 'w') as f:
+                    json.dump(v, f)
+            elif not os.path.exists(os.path.join(root, 'results', str(idx), f'{k}.txt')):
+                with open(os.path.join(root, 'results', str(idx), f'{k}.txt'), 'w') as f:
+                    f.write(str(v))
 
 def generate(df, generator, root='./'):
     df = df.copy()
@@ -32,6 +44,8 @@ if __name__ == '__main__':
         req_gen = requester.OllamaRequester(GENERATE_MODEL_NAME, GENERATE_PROMPT_NAME)
     
     df = pd.read_json('augmented-clinical-notes/augmented_notes_30K.jsonl', lines=True)
-    if IDXS is not 'all':
+    if IDXS != 'all':
         df = df[df['idx'].isin(IDXS)].reset_index(drop=True)
+    df = df.set_index('idx')
+    init_dirs(df, root='./')
     generate(df, req_gen)
